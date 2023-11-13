07a5e516f39a90827df59c6bd865f5b41452ae98
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-11-13 14:02:57 +0530

    my new 2nd nestapplication

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+@Entity()
+export class User {
+       @PrimaryGeneratedColumn()
+       id : number;
+
+       @Column({length:100})
+        name: string;
+
+        @Column({length:100})
+        email: string;
+
+        @Column({length:100})
+        password: string;
+}
